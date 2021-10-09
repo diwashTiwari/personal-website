eda772ecdcbb9348b68febd12b5a4dca021a9d73
Leo <leodiwas@icloud.com>
2021-10-09 16:38:44 +0545

    added files

---
+++ Documents/Projects/Education-Website/script.js
@@ -0,0 +1,28 @@
+// define all UI variable
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.navbar-content ul');
+const navLinks = document.querySelectorAll('.navbar-content a');
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
+
+
+
+
+font-weight: ;
+font-size: ;
+line-height: ;
+color: ;
