7b7dd497ac7ebb2d4ddc0509c1c058818b5bf930
Leo <leodiwas@icloud.com>
2021-10-14 00:04:11 +0545

    responsive codes implemented

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let btn = document.getElementById("click-btn");
-btn.addEventListener('click', function() {
-    alert("Namaste");
-}) ;
+// define all UI variable
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.navbar-content ul');
+const navLinks = document.querySelectorAll('.navbar-content a');
 
+// load all event listners
+allEventListners();
 
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
